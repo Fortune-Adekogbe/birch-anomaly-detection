fa5d8ee3bf023ef62704933ae5b82603f673771a
fortune-adekogbe <fortuneadekogbe@gmail.com>
2022-03-31 09:27:46 +0100

    formatted script, added doc strings.

--- birch-anomaly.py
+++ birch-anomaly.py
@@ -10,16 +10,18 @@ from sklearn.metrics import confusion_matrix
 
 HOST = 'http://localhost:9000'
 
+
 def execute_query(query_sql: str) -> pd.DataFrame:
     """
     This function takes in a string query_sql representing an SQL query and returns the results as a dataframe
     """
-    query_params = {'query': query_sql, 'fmt' : 'json'}
+    query_params = {'query': query_sql, 'fmt': 'json'}
     try:
         response = requests.get(HOST + '/exec', params=query_params)
         json_response = json.loads(response.text)
-        skab_df = pd.DataFrame(data=json_response['dataset'], columns=[i['name'] for i in json_response['columns']])
-        
+        skab_df = pd.DataFrame(data=json_response['dataset'], columns=[
+                               i['name'] for i in json_response['columns']])
+
         print(skab_df.shape)
         return skab_df
     except KeyError:
@@ -27,37 +29,48 @@ def execute_query(query_sql: str) -> pd.DataFrame:
     except requests.exceptions.RequestException as e:
         print(f"Error: {e}")
 
+
 def process_data(df: pd.DataFrame) -> tuple:
+    '''
+    This function cleans and processes an input dataframe and returns a tuple containing the 
+    cleaned dataframe and the target column.
+    '''
     del df['changepoint']
-    df = df.astype({'anomaly':'float'}).astype({'anomaly':'int'})
+    df = df.astype({'anomaly': 'float'}).astype({'anomaly': 'int'})
     df.set_index('datetime', inplace=True)
     anomaly = df['anomaly']
     del df['anomaly']
     return df, anomaly
+
 
 def get_labels(labels: list) -> list:
     """
     This function returns the anomaly label of the datapoints.
     """
     l, c = np.unique(labels, return_counts=True)
-    print(l,c)
+    print(l, c)
     mini = l[np.argmin(c)]
-    return np.array([i==mini for i in labels], dtype='int64')
+    return np.array([i == mini for i in labels], dtype='int64')
+
 
 def plot_anomaly_distribution(labels: list, df: pd.DataFrame) -> None:
     """
     This function plots the anomaly distribution of the input dataframe given the labels.
     """
-    X= PCA(n_components=2).fit_transform(df)
-    plt.scatter(X[:,0], X[:,1], c=labels, cmap='rainbow', alpha=0.7, edgecolors='b')
+    X = PCA(n_components=2).fit_transform(df)
+    plt.scatter(X[:, 0], X[:, 1], c=labels,
+                cmap='rainbow', alpha=0.7, edgecolors='b')
     plt.title("Anomaly Distribution on SKAB Data using BIRCH")
     plt.show()
 
+
 # create dataframe
-skab_df = execute_query("SELECT * FROM alldata_skab.csv WHERE anomaly IN('0.0', '1.0');") #  
+skab_df = execute_query(
+    "SELECT * FROM alldata_skab.csv WHERE anomaly IN('0.0', '1.0');")
 skab_df, anomaly = process_data(skab_df)
 
-birch_model = Birch(n_clusters=2,threshold=1.5,)
+# crete model
+birch_model = Birch(n_clusters=2, threshold=1.5,)
 birch_model.fit(skab_df)
 labels = birch_model.labels_
 
@@ -65,5 +78,5 @@ pred = get_labels(labels)
 cm = confusion_matrix(anomaly, pred)
 print(f"Anomalies detected: {cm[1][1]/sum(cm[1])}")
 
-#plot result
+# plot result
 plot_anomaly_distribution(labels, skab_df)
